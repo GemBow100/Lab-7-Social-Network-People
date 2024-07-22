65ed6d149bffd282984f0bacc51cd1f04f675f94
Joelle Waugh <joelle.waugh@flemingcollege.ca>
2024-07-22 09:35:07 -0400

    Update old_people.py

--- old_people.py
+++ old_people.py
@@ -10,6 +10,8 @@ import os
 from create_db import db_path, script_dir
 import sqlite3
 from pprint import pprint
+from openpyxl import Workbook
+import pandas
 
 def main():
     old_people_list = get_old_people()
@@ -31,7 +33,7 @@ def get_old_people():
 # Fetch all query results.
 # The fetchall() method returns a list, where each list item
 # is a tuple containing data from one row in the people table.
-    all_people = cur.fetchall()
+    all_people = cur.fetchall('name, age')
 # Pretty print (pprint) outputs data in an easier to read format.
     pprint(all_people)
     con.commit()
@@ -46,17 +48,30 @@ def print_name_and_age(name_and_age_list):
         name_and_age_list (list): (name, age) of people
     
     """
-    
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
     for row in cur.execute ("Select name and age t FROM data"):
-            cur.execute(name_and_age_list)
         print (row)
-    rows = [
-        ("row1",),
-        ("row2",),
-    ]
-    cur.excutemany("Insert into data values(?)", rows)
-    
-    return
+        rows = [
+        ("Name",),
+        ("Age",),
+        ]
+        cur.excutemany("Insert into data values(?)", rows)
+        cur.executescript="""
+        CREATE TABLE
+        (
+            name,
+            age,
+
+        )
+        """
+
+
+        cur.execute(rows, db_path)
+        con.commit
+    con.close()
+    return name_and_age_list
 
 def save_name_and_age_to_csv(name_and_age_list, csv_path):
     """Saves name and age of all people in provided list
@@ -65,16 +80,24 @@ def save_name_and_age_to_csv(name_and_age_list, csv_path):
         name_and_age_list (list): (name, age) of people
         csv_path (str): Path of CSV file
     """
-    cur.executescript("""
-    
-    CREATE TABLE person(firstname, lastname, age);
-    CREATE TABLE publisher(name, address);
-""")
-    con = sqlite3.connect(":memory:")
-    cur = con.cursor()
-    cur.connection == con
-    True
-    con.close()
+
+    wb = Workbook(csv_path)
+
+# grab the active worksheet
+    ws = wb.active
+
+# Data can be assigned directly to cells
+    ws['name'] = name_and_age_list
+
+# Rows can also be appended
+    ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+
+    ws['age'] = name_and_age_list
+
+# Save the files
+    wb.save("Social Network")
     return
 
 if __name__ == '__main__':
